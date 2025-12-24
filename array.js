523c76a31e32415a1592c0e85fe6dbe4c1c25bc5
Ravi Shankar Mehta <ravi.mehta@atpl.live>
2025-12-24 10:10:18 +0530

    js imp consept

--- array.js
+++ array.js
@@ -108,13 +108,15 @@
 // });
 
 //Example 1: Find Max Element
-function findMax(arr) {
-  let max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
-console.log(findMax([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// function findMax(arr) {
+//   let max = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+// console.log(findMax([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+console.log(("b" + "a" + +"a" + "a").toLocaleLowerCase());
--- jsInterview.js
+++ jsInterview.js
@@ -4,3 +4,107 @@ console.log(encoded_uri);
 // let decoded_uri = decodeURI(encoded_uri);
 
 // console.log(decoded_uri);
+
+// JavaScript Concepts
+
+// 1. Scope of variables (const, let and var)
+
+// 2. Function & Variable hoisting
+
+// 3. Closures
+
+// 4. Callback Hell
+
+// 5. Asynchronous vs Synchronous (How to implement both in JS)
+
+// 6. 'this' variable in Javascript
+
+// 7. Promises
+
+// 8. Function.prototype & Inheritance in JavaScript
+
+// 9. Call, Apply, Bind methods in JavaScript
+
+// 10. Polyfill for bind()
+
+// 11. Currying in JavaScript
+
+// 12. localStorage vs sessionStorage
+
+// 13. CORS
+
+// 14. Event Loop
+
+// 15. ES6 - Arrow function + why do we need them?
+
+// 16. Cookies & how do they work?
+
+// 17. Debouncing in JavaScript
+
+// 18. Throttling in JavaScript
+
+// 19. Debouncing vs Throttling
+
+// 20. ES6 modules
+
+// 21. web workers (& shared workers) and service workers
+
+// 22. Async/Await
+
+// 23. How do you add a poly-fill
+
+// 24. Event bubbling/capturing
+
+// 25. Event Delegation
+
+// 26. functional programming with javascript
+
+// 27. use strict in JavaScript
+
+// 28. object.freeze in JavaScript
+
+// 29. LESS
+
+// 30. SCSS
+
+// 31. SCSS vs SASS
+
+// 32. super keyword in JavaScript
+
+// 33. Lodash library ar
+
+// 34. webpack
+
+// 35. Modern ES6 features (spread operator, destructuring, etc.)
+
+// 36. GraphQL
+
+// 37. Testing in JavaScript with JEST
+
+// 38. different methods of Array object in Javascript
+
+// 39. Redux in React
+
+// 40. Context API in React
+
+// 41. Hooks in React (useState)
+
+// 42. Component Lifecycle Hooks
+
+// 43. Creating object clone in JS (Object.assign
+
+// 44. Shallow copy vs. Deep copy
+
+// 45. JWT
+
+// 46. Reference vs Value in JavaScript
+
+// 47. Async vs Defer
+// AR Don Duch Chift and Inchift Array Mathode in lauscarist
+// Shallow copy vs. Deep copy
+
+// 48. Pop, Push, Shift and Unshift Array Methods in JavaScript
+
+// 49. String.slice() vs String.substring() vs String.substr()
+
+// 50. Array slice vs splice in JavaScript
